5ed632af9368227f5ad0f4ede740c8dc3cb3bbf0
crew52 <dungnc150602@gmail.com>
2024-11-29 11:17:15 +0700

    add class customer, enum TypeAccount

---
+++ entity/customer.js
@@ -0,0 +1,57 @@
+class Customer {
+    _customerId;
+    _customerName;
+    _account;
+
+    constructor(customerId, customerName) {
+        this._customerId = customerId;
+        this._customerName = customerName;
+        this._account = null;
+    }
+
+    getCustomerId() {
+        return this._customerId;
+    }
+
+    setCustomerId(value) {
+        this._customerId = value;
+    }
+
+    getCustomerName() {
+        return this._customerName;
+    }
+
+    setCustomerName(value) {
+        this._customerName = value;
+    }
+
+    getAccounts() {
+        return this._account;
+    }
+
+    setAccount(account) {
+        if (account instanceof Account) {
+            if (this._account === null) {
+                this._account = account;
+                console.log(`Tài khoản ${account.getAccountNumber()} đã được gắn với khách hàng ${this._customerName}`);
+            } else {
+                console.log(`Khách hàng ${this._customerName} đã sở hữu tài khoản. Không thể gắn thêm.`);
+            }
+        } else {
+            console.log("Đối tượng không phải là tài khoản hợp lệ");
+        }
+    }
+
+    removeAccount() {
+        if (this._account) {
+            console.log(`Tài khoản ${this._account.getAccountNumber()} đã bị xóa khỏi khách hàng ${this._customerName}`);
+            this._account = null;
+        } else {
+            console.log(`Khách hàng ${this._customerName} không có tài khoản để xóa`);
+        }
+    }
+
+    hasAccount(accountNumber) {
+        return this._account && this._account.getAccountNumber() === accountNumber;
+    }
+}
