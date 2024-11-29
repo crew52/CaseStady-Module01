c9f6a6948e18237c9904e8df84b18a40aa532caf
crew52 <dungnc150602@gmail.com>
2024-11-29 14:23:21 +0700

    add new fun showErrorMessage, showCustomerForm, addCustomer

--- src/funcBank.js
+++ src/funcBank.js
@@ -22,6 +22,24 @@ rew2.deposit(100000);
 son2.deposit(100000);
 
 showCustomers();
+
+function showErrorMessage(message) {
+    const messageDiv = document.getElementById("message");
+    messageDiv.textContent = message;
+    messageDiv.style.display = "block"; // Hiển thị thông báo lỗi
+}
+
+// show customer form
+function showCustomerForm() {
+    const form = document.getElementById('customer-form');
+    // Kiểm tra trạng thái hiển thị hiện tại của form
+    if (form.style.display === 'none' || form.style.display === '') {
+        form.style.display = 'block'; // Hiển thị form
+    } else {
+        form.style.display = 'none'; // Ẩn form
+    }
+}
+
 // display danh sach customer
 function  showCustomers() {
     const customerList = document.getElementById('customer-list');
@@ -29,11 +47,21 @@ function  showCustomers() {
     bank._customers.forEach(customer => {
         const listItem = document.createElement('li');
         listItem.textContent = `${customer._customerName} (ID: ${customer._customerId})`;
-        listItem.onclick = () => showAccounts(customer._customerId);
+        // listItem.onclick = () => showAccounts(customer._customerId);
         customerList.appendChild(listItem);
     })
 }
 
+// Thêm khách hàng mới
+function addCustomer() {
+    const customerName = document.getElementById("customer-name").value;
+    const customerId = `C${Date.now()}`;
+    const newCustomer = new Customer(customerId, customerName);
+    bank.addCustomer(newCustomer);
+    document.getElementById("customer-name").value = ""; // Xóa nội dung ô nhập
+    showCustomers();
+    // updateCustomerDropdown();
+}
 
 
 
